479cda2542b453a47b9f9d6e0bd3721273a4de48
elueckel <30531515+elueckel@users.noreply.github.com>
2020-01-05 12:19:48 +0100

    Update module.php

--- ControlByWeather/module.php
+++ ControlByWeather/module.php
@@ -576,8 +576,8 @@ if (!defined('vtBoolean')) {
 		
 		public function CBWNotifyApp()
 		{
-			$this->GetBuffer("NotifierTitle", $NotifierTitle);
-			$this->GetBuffer("NotifierMessage", $NotifierMessage);
+			$NotifierTitle = $this->GetBuffer("NotifierTitle");
+			$NotifierMessage = $this->GetBuffer("NotifierMessage");
 			//NotifierTitle - NotifierMessage
 			//$WebFrontMobile = $this->ReadPropertyInteger("WebFrontMobile");
 			$WebFrontMobile = IPS_GetInstanceListByModuleID('{3565B1F2-8F7B-4311-A4B6-1BF1D868F39E}')[0];
