c4349e6e795cb19cc4fe46c1aeee85d5a99d0221
elueckel <30531515+elueckel@users.noreply.github.com>
2020-01-12 11:42:46 +0100

    Update module.php

--- ControlByWeather/module.php
+++ ControlByWeather/module.php
@@ -1355,7 +1355,7 @@ if (!defined('vtBoolean')) {
 			
 			$SystemIsDay = GetValue($this->ReadPropertyInteger("SystemIsDay")); // check if is day
 			
-			$SystemPresence = GetValue($this->ReadPropertyInteger("SystemPresence"))
+			$SystemPresence = GetValue($this->ReadPropertyInteger("SystemPresence"));
 			
 			if (empty ($SystemPresence)) {
 				$SystemPresence = 1;
